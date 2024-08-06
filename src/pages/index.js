aad5bbff4f674ac8678f842ca387c3ef3b44555d
Chamika Sandun <110515504+Dodamwaththacs@users.noreply.github.com>
2024-08-06 21:32:04 +0530

    set a image and divide page in to two

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col min-h-screen">
       <Head>
         <title>My Portfolio</title>
         <meta name="description" content="My personal portfolio website" />
@@ -14,12 +14,53 @@ export default function Home() {
 
       <Header />
 
-      <main className={styles.main}>
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is the homepage of my portfolio.</p>
+      <main>
+        <div class="flex flex-row">
+          <div class="basis-1/2">
+            <h1 className="text-4xl font-bold">Hello, I'm ZERO_31</h1>
+            <p className="text-lg text-gray-600">
+              I'm a web developer based in Sri Lanka
+            </p>
+          </div>
+          <div class="basis-1/2 flex justify-end">
+            <div className="">
+              <Image
+                src="/myphoto.png"
+                alt="Profile Picture"
+                width={400}
+                height={700}
+                quality={100}
+                priority
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
       </main>
 
       <Footer />
     </div>
   );
+}
+
+{
+  /* <div className="flex justify-between items-center">
+          <div className="text-left w-1/2">
+            <h1 className="text-4xl font-bold">Hello, I'm ZERO_31</h1>
+            <p className="text-lg text-gray-600">
+              I'm a web developer based in Sri Lanka
+            </p>
+          </div>
+          <div className="w-48 h-48 relative">
+            <Image
+              src="/myphoto.png"
+              alt="Profile Picture"
+              width={400}
+              height={700}
+              quality={100}
+              priority
+              className="rounded-full shadow-lg object-cover"
+            />
+          </div>
+        </div> */
 }
